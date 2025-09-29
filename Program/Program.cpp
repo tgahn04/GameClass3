f96c31890020c767af6c06ec5c3ee3e8e8efa16a
tgahn04 <tgahn0524@gmail.com>
2025-09-29 19:47:03 +0900

    (2025.9.29) 선형 컨테이너

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,9 +1,59 @@
 ﻿#include "stdafx.h"
-
+#include <vector>
+#include <list>
 
 int main()
 {
-#pragma region 
+#pragma region 선형 컨테이너
+	// 데이터를 선형으로 저장하며, 특별한 제약이나 규칙이 없는 컨테이너입니다.
+		
+#pragma region vector container
+	
+	// vector<int> vector;
+	
+	// vector.reserve(8);
+	
+	// vector.push_back(10);
+	// vector.push_back(20);
+	// vector.push_back(30);
+	// vector.push_back(40);
+	// vector.push_back(50);
+	
+	// for (int i = 0; i < vector.size(); i++)
+	// {
+	// 	cout << "vector의 값 : " << vector[i] << endl;
+	// }
+	
+	// vector.clear();
+	
+	// cout << "vector의 size 값 : " << vector.size() << endl;
+	// cout << "vector의 capacity 값 : " << vector.capacity() << endl;
+
+
+
+#pragma endregion
+
+#pragma region list container
+	list<int> list;
+
+	
+	list.push_back(20);
+	list.push_back(30);
+	
+	list.push_front(10);
+	
+	list.pop_front();
+
+	list.push_front(1);
+
+	list.remove(20);
+
+	for (const int container : list)
+	{
+		cout << "list의 값 : " << container << endl;
+	}
+#pragma endregion
+
 
 #pragma endregion
 
